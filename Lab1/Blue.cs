51256b2177d2d33879f1bcf2a14ac1a128e71415
max <skomadax@gmail.com>
2026-09-08 17:40:40 +0300

    тестовый коммит

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab1
+namespace Lab1
 {
     public class Blue
     {
@@ -6,11 +6,23 @@
         {
             bool answer = false;
 
-            // code here
-
-            // end
-
-            return answer;
+            if (a>0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            if (b>0)
+            {
+                answer = true;
+            }
+            else 
+            { 
+                answer = false;
+            }
+                return answer;
         }
         public bool Task2(double d)
         {
